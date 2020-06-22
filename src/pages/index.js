d96523d97b169dd33b6839106190fbc42eac4c78
Anton Kristiono <antonkristiono47@gmail.com>
2020-06-22 20:48:56 +0700

    button WA Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,31 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <SEO title="Home" />
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div>
+          <div>
+            <a href="https://api.whatsapp.com/send?phone=+6289647322312&text=Halo">
+              <button>
+                Whatsapp Kami
+              </button>
+            </a>
+          </div>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        </Layout>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
